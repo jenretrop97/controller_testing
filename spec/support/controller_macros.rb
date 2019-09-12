c8333d74460e01be75121810cee6709719bb6834
Jennifer Porter <jlporter@myhiddencove.com>
2019-09-11 19:00:53 -0600

    controller_tesing

---
+++ spec/support/controller_macros.rb
@@ -0,0 +1,9 @@
+module ControllerMacros
+  def login_user
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mapping[:user]
+      @user = User.create(email: 'test@test.com', password: 'password', first_name: 'Test', last_name: 'Tester', age: 27)
+      sign_in @user
+    end
+  end
+end
